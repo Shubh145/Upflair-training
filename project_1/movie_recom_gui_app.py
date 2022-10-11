d121ad564c9519a6e5699ee5a0c2c96b88faf61d
shubh145 <shubh.xdr@gmail.com>
2022-10-12 02:53:27 +0530

    done

--- project_1/movie_recom_gui_app.py
+++ project_1/movie_recom_gui_app.py
@@ -1,4 +1,4 @@
-from select import select
+
 import tkinter as ttk
 import pandas as pd
 import warnings
@@ -8,12 +8,14 @@ print('Location is:',os.getcwd(), '\n\n\n')
 
 app = ttk.Tk()
 app.title('Recommendation system')
-app.geometry('700x400')
+app.geometry('800x500')
 
 cols = ['user_id','movie_id','rating','ts']
 df = pd.read_csv('u.data', sep='\t', names=cols).drop('ts',axis=1)
 item_cols = ['movie_id','title'] + [str(i) for i in range(22)]
 df1 = pd.read_csv('u.item', sep='|', names = item_cols, encoding = "ISO-8859-1")[['movie_id','title']]
+
+#merging two DataFrame
 movie = pd.merge(df,df1, on='movie_id')
     
 
@@ -25,17 +27,19 @@ frame.place(x=10,y=10)
 box=ttk.Listbox(frame,height=10,width=50)
 for title in movie['title'].unique():
     box.insert(ttk.END,title)
-#box.groid(row=0,column=0)
+#box.grid(row=0,column=0)
 box.pack(side='left',fill='y')
 #box.place(x=10,y=10)
 
+#scroll bar adding
 scroll=ttk.Scrollbar(frame,orient=ttk.VERTICAL)
 scroll.config(command=box.yview)
 box.config(yscrollcommand=scroll.set)
 scroll.pack(side='right',fill='y')
 
 def get_movie():
-    movie_selected=box.get(box.curselection)
+    pass
+    movie_selected=box.get(box.curselection())
     print('movie_selected:',movie_selected)
 
     
@@ -50,30 +54,29 @@ def get_movie():
     corrs_df['count'] = movie['title'].value_counts()
     
     # Find Top 2-3 Recommendations
-    top_recom = list(corrs_df[corrs_df['count']>50].sort_values(
-        by='correlation', ascending=False).head(3).index)
+    top_recom = list(corrs_df[corrs_df['count']>50].sort_values(by='correlation', ascending=False).head(3).index)
     
     #important
     if movie_selected in top_recom:
         top_recom.remove(movie_selected)
     print('Recommendation:',top_recom)
     
-   # if top_recom:
-  #      result.set(top_recom[0])
-  #  else:
-   #     result.set('sorry')
+ 
+    #top_recom.remove(movie_selected)
+   # print('Recommendation:',top_recom)
 
+#   result.set(top_recom[0])
 
-    top_recom.remove(movie_selected)
-    print('Recommendation:',top_recom)
+  #  top_recom.remove(movie_selected)
+   # print('Recommendation:',top_recom)
 
-    result.set(top_recom[0])
+    if top_recom:
+        result.set(top_recom[0])
+    else:
+        result.set('sorry no recommendation found ! ')
 
-    top_recom.remove(movie_selected)
-    print('Recommendation:',top_recom)
-
-ttk.Button(app,text='Find Reommendation',font=('Ariel',22),command=get_movie).place(x=350,y=10)
-ttk.Label(app,textvariable=result,font=('Ariel',22),command=get_movie).place(x=10,y=300)
+ttk.Button(app,text='Find Reommendation',font=('Ariel',22),command=get_movie).place(x=10,y=250)
+ttk.Label(app,textvariable=result,font=('Ariel',22)).place(x=10,y=350)
 
 app.mainloop()
 
